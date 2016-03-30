34781f3e0d51df955c42cadccc64fc29dea2bc65
dombag85 <dombag85@gmail.com>
2016-03-29 22:50:33 -0600

    Version 1 of shopping cart.  Function only, no classes or objects.

--- shoppingCart/main.cpp
+++ shoppingCart/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void message()
+{
+    cout << "Welcome to Dombag's Store!" << endl;
+}
+
+float func(float x, float y)
+{
+    float price;
+    float tax;
+    float total;
+    
+    price = (x * y);
+    tax = (0.06 * price);
+    total = price + tax;
+    
+    return total;
+}
+
+int main()
+{
+    float z;
+    int a;
+    int b;
+    
+    message();
+    cout << "Enter the quantity of CDs " << endl;
+    cin >> a;
+    cout << "Enter the price " << endl;
+    cin >> b;
+    
+    z =func(a, b);
+    cout << fixed;
+    cout << setprecision(2);
+    cout << "The cost of your two items is " << z << endl;
     return 0;
 }
